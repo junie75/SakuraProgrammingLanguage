e736fb506a18b61cb59c6322f7e55a9c5a39fdef
juni <juniejere6654@gmail.com>
2023-09-28 16:13:13 -0500

    Added code for scanner

--- SakuraCompiler.cpp
+++ SakuraCompiler.cpp
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------
-// Juni Ejere & Dr. Art Hanna 
-// Sakura Reader "driver" program
-// SakuraReader.cpp
+// Juni Ejere & Dr. Art Hanna
+// Sakura1 Scanner
+// Sakura1Scanner.cpp
 //-----------------------------------------------------------
 #include <iostream>
 #include <iomanip>
@@ -16,9 +16,85 @@
 
 using namespace std;
 
-#define TRACEREADER
+//#define TRACEREADER
+#define TRACESCANNER
 
 #include "Sakura.h"
+
+//-----------------------------------------------------------
+typedef enum
+//-----------------------------------------------------------
+{
+// pseudo-terminals
+   IDENTIFIER,
+   STRING,
+   EOPTOKEN,
+   UNKTOKEN,
+// reserved words
+   MAIN,
+   OUTPUT,
+   ENDL,
+// punctuation
+   COLON,
+   OUTSHIFT, //<< ------ is it operator? 
+// operators
+// ***NONE***
+} TOKENTYPE;
+
+//-----------------------------------------------------------
+struct TOKENTABLERECORD
+//-----------------------------------------------------------
+{
+   TOKENTYPE type;
+   char description[12+1];
+   bool isReservedWord;
+};
+
+//-----------------------------------------------------------
+const TOKENTABLERECORD TOKENTABLE[] =
+//-----------------------------------------------------------
+{
+   { IDENTIFIER  ,"IDENTIFIER"  ,false },
+   { STRING      ,"STRING"      ,false },
+   { EOPTOKEN    ,"EOPTOKEN"    ,false },
+   { UNKTOKEN    ,"UNKTOKEN"    ,false },
+   { MAIN        ,"main"        ,true  },
+   { OUTPUT      ,"output"      ,true  },
+   { ENDL        ,"ENDL"        ,false }, //endl is not a reservedword so false, ^n symbol is?
+   { OUTSHIFT    ,"OUTSHIFT"    ,false },
+   { COLON       ,"COLON"       ,false }
+   //NEED TO ADD SOMETHING FOR END!!!! FOR THE PARSING FUNCTION
+   //JK can just use EOPTOKEN
+};
+
+//-----------------------------------------------------------
+struct TOKEN
+//-----------------------------------------------------------
+{
+   TOKENTYPE type;
+   char lexeme[SOURCELINELENGTH+1];
+   int sourceLineNumber;
+   int sourceLineIndex;
+};
+
+//--------------------------------------------------
+// Global variables
+//--------------------------------------------------
+READER<CALLBACKSUSED> reader(SOURCELINELENGTH,LOOKAHEAD);
+LISTER lister(LINESPERPAGE);
+
+//--------------------------------------------------
+void ProcessCompilerError(int sourceLineNumber,int sourceLineIndex,const char errorMessage[])
+//--------------------------------------------------
+{
+   char information[SOURCELINELENGTH+1];
+
+// Use "panic mode" error recovery technique: report error message and terminate compilation!
+   sprintf(information,"     At (%4d:%3d) %s",sourceLineNumber,sourceLineIndex,errorMessage);
+   lister.ListInformationLine(information);
+   lister.ListInformationLine("Sakura compiler ending with compiler error!\n");
+   throw( SAKURAEXCEPTION("Sakura compiler ending with compiler error!") );
+}
 
 //-----------------------------------------------------------
 int main()
@@ -26,16 +102,11 @@ int main()
 {
    void Callback1(int sourceLineNumber,const char sourceLine[]);
    void Callback2(int sourceLineNumber,const char sourceLine[]);
+   void GetNextToken(TOKEN tokens[]);
 
-   char sourceFileName[80+1]; //name of the file
-   NEXTCHARACTER nextCharacter; //holds the value of the next character in the source code
-
-   // READER<CALLBACKSUSED> reader(SOURCELINELENGTH,LOOKAHEAD);
-   // READER<CALLBACKSUSED> reader(3,LOOKAHEAD);
-   LISTER lister(LINESPERPAGE); //LISTER class outputs page-formatted information to the list file as a “log” of the translation process of a source file
-   READER<CALLBACKSUSED> reader(SOURCELINELENGTH,LOOKAHEAD); //READER class provides one-character-at-a-time, top-to-bottom, left-to-right traversal of the characters contained in the source file. 
-
-    /*Input the source file to be read*/
+   char sourceFileName[80+1];
+   TOKEN tokens[LOOKAHEAD+1];
+   
    cout << "Source filename? ";
    cin >> sourceFileName;
 
@@ -47,22 +118,24 @@ int main()
       reader.AddCallbackFunction(Callback2);
       reader.OpenFile(sourceFileName);
 
-    /*while have not reached the end of the program, read each character*/
-      do
-      {
-         nextCharacter = reader.GetNextCharacter();
-      } while ( nextCharacter.character != READER<CALLBACKSUSED>::EOPC ); //EOPC = end of program character, defined in Sakura.h
-    // } while ( nextCharacter.character != READER::EOPC );
+   // Fill tokens[] for look-ahead
+      for (int i = 0; i <= LOOKAHEAD; i++)
+         GetNextToken(tokens);
+
+   // Scan entire source file (causes outputting of TRACESCANNER-enabled information to list file)
+      while ( tokens[0].type != EOPTOKEN )
+         GetNextToken(tokens);
    }
    catch (SAKURAEXCEPTION sakuraException)
    {
       cout << "Sakura exception: " << sakuraException.GetDescription() << endl;
    }
-   lister.ListInformationLine("******* Sakura reader ending");
-   cout << "Sakura reader ending\n";
+   lister.ListInformationLine("******* Sakura1 scanner ending");
+   cout << "Sakura1 scanner ending\n";
 
    system("PAUSE");
    return( 0 );
+   
 }
 
 //-----------------------------------------------------------
@@ -77,4 +150,274 @@ void Callback2(int sourceLineNumber,const char sourceLine[])
 //-----------------------------------------------------------
 {
    cout << sourceLine << endl;
+}
+
+//-----------------------------------------------------------
+void GetNextToken(TOKEN tokens[])
+//-----------------------------------------------------------
+{
+   const char *TokenDescription(TOKENTYPE type);
+
+   int i;
+   TOKENTYPE type;
+   char lexeme[SOURCELINELENGTH+1];
+   int sourceLineNumber;
+   int sourceLineIndex;
+   char information[SOURCELINELENGTH+1];
+
+//============================================================
+// Move look-ahead "window" to make room for next token-and-lexeme
+//============================================================
+   for (int i = 1; i <= LOOKAHEAD; i++)
+      tokens[i-1] = tokens[i];
+
+   char nextCharacter = reader.GetLookAheadCharacter(0).character;
+
+//============================================================
+// "Eat" white space and comments
+//============================================================
+   do
+   {
+//    "Eat" any white-space (blanks and EOLCs and TABCs) 
+      while ( (nextCharacter == ' ')
+           || (nextCharacter == READER<CALLBACKSUSED>::EOLC)
+           || (nextCharacter == READER<CALLBACKSUSED>::TABC) )
+         nextCharacter = reader.GetNextCharacter().character;
+
+//    "Eat" line comment
+//    --line comment
+      if ( (nextCharacter == '-' ) && (reader.GetLookAheadCharacter(1).character == '-') )
+      {
+
+#ifdef TRACESCANNER
+   sprintf(information,"At (%4d:%3d) begin line comment",
+      reader.GetLookAheadCharacter(0).sourceLineNumber,
+      reader.GetLookAheadCharacter(0).sourceLineIndex);
+   lister.ListInformationLine(information);
+#endif
+
+         do
+            nextCharacter = reader.GetNextCharacter().character;
+         while ( nextCharacter != READER<CALLBACKSUSED>::EOLC );
+      } 
+
+//    "Eat" block comments (nesting allowed) 
+//     (-: 'blocked comment' :-)
+      if ( (nextCharacter == '(') && (reader.GetLookAheadCharacter(1).character == '-') && (reader.GetLookAheadCharacter(2).character == ':' ) )
+      {
+         int depth = 0;
+
+         do
+         {
+            if ( (nextCharacter == '(') && (reader.GetLookAheadCharacter(1).character == '-') && (reader.GetLookAheadCharacter(2).character == ':' ) )
+            {
+               depth++;
+
+#ifdef TRACESCANNER
+   sprintf(information,"At (%4d:%3d) begin block comment depth = %d",
+      reader.GetLookAheadCharacter(0).sourceLineNumber,
+      reader.GetLookAheadCharacter(0).sourceLineIndex,
+      depth);
+   lister.ListInformationLine(information);
+#endif
+
+               nextCharacter = reader.GetNextCharacter().character;
+               nextCharacter = reader.GetNextCharacter().character;
+            }
+            else if ( (nextCharacter == ':') && (reader.GetLookAheadCharacter(1).character == '-') && (reader.GetLookAheadCharacter(2).character == ')') )
+            {
+
+#ifdef TRACESCANNER
+   sprintf(information,"At (%4d:%3d)   end block comment depth = %d",
+      reader.GetLookAheadCharacter(0).sourceLineNumber,
+      reader.GetLookAheadCharacter(0).sourceLineIndex,
+      depth);
+   lister.ListInformationLine(information);
+#endif
+
+               depth--;
+               nextCharacter = reader.GetNextCharacter().character;
+               nextCharacter = reader.GetNextCharacter().character;
+               nextCharacter = reader.GetNextCharacter().character;
+            }
+            else
+               nextCharacter = reader.GetNextCharacter().character;
+         }
+         while ( (depth != 0) && (nextCharacter != READER<CALLBACKSUSED>::EOPC) );
+         if ( depth != 0 ) 
+            ProcessCompilerError(reader.GetLookAheadCharacter(0).sourceLineNumber,
+                                 reader.GetLookAheadCharacter(0).sourceLineIndex,
+                                 "Unexpected end-of-program");
+      }
+   } while ( (nextCharacter == ' ')
+          || (nextCharacter == READER<CALLBACKSUSED>::EOLC)
+          || (nextCharacter == READER<CALLBACKSUSED>::TABC)
+          || ((nextCharacter == '-') && (reader.GetLookAheadCharacter(1).character == '-'))
+          || ((nextCharacter == '(') && (reader.GetLookAheadCharacter(1).character == '-') && (reader.GetLookAheadCharacter(2).character == ':')) );
+
+//============================================================
+// Scan token
+//============================================================
+   sourceLineNumber = reader.GetLookAheadCharacter(0).sourceLineNumber;
+   sourceLineIndex = reader.GetLookAheadCharacter(0).sourceLineIndex;
+
+// reserved words (and <identifier> ***BUT NOT YET***)
+// as soon as first letter is read, looks ahead to find entire lexeme & checks if it exists in tokentable
+   if ( isalpha(nextCharacter) )
+   {
+      //char UCLexeme[SOURCELINELENGTH+1];
+
+      i = 0;
+      lexeme[i++] = nextCharacter;
+      nextCharacter = reader.GetNextCharacter().character;
+      while ( isalpha(nextCharacter) || isdigit(nextCharacter) || (nextCharacter == '_') ) //fills lexeme until it encounters whitespace or invalid symbol
+      {
+         lexeme[i++] = nextCharacter;
+         nextCharacter = reader.GetNextCharacter().character;
+      }
+      lexeme[i] = '\0';
+      /*for (i = 0; i <= (int) strlen(lexeme); i++)
+         UCLexeme[i] = toupper(lexeme[i]);*/
+
+      bool isFound = false;
+
+      i = 0;
+      while ( !isFound && (i <= (sizeof(TOKENTABLE)/sizeof(TOKENTABLERECORD))-1) ) //checks if lexeme is a reserved word
+      {
+         //if ( TOKENTABLE[i].isReservedWord && (strcmp(UCLexeme,TOKENTABLE[i].description) == 0) )
+         if ( TOKENTABLE[i].isReservedWord && (strcmp(lexeme,TOKENTABLE[i].description) == 0) )
+            isFound = true;
+         else
+            i++;
+      }
+      if ( isFound )
+         type = TOKENTABLE[i].type;
+      else
+         type = IDENTIFIER;
+   }
+   else
+   {
+      switch ( nextCharacter )
+      {
+// <string>
+         case '`': 
+            i = 0;
+            nextCharacter = reader.GetNextCharacter().character;
+            while ( (nextCharacter != '`') && (nextCharacter != READER<CALLBACKSUSED>::EOLC) )
+            {
+            	//check for escape sequences for \" or \\ to print '"' or '\' in a string
+            	//don't need because '`' delimits a string
+               /*if      ( (nextCharacter == '\\') && (reader.GetLookAheadCharacter(1).character == '"') )
+               {
+                  lexeme[i++] = nextCharacter;
+                  nextCharacter = reader.GetNextCharacter().character;
+               }
+               else if ( (nextCharacter == '\\') && (reader.GetLookAheadCharacter(1).character == '\\') )
+               {
+                  lexeme[i++] = nextCharacter;
+                  nextCharacter = reader.GetNextCharacter().character;
+               }*/ 
+               lexeme[i++] = nextCharacter;
+               nextCharacter = reader.GetNextCharacter().character;
+            }
+            if ( nextCharacter == READER<CALLBACKSUSED>::EOLC )
+               ProcessCompilerError(sourceLineNumber,sourceLineIndex,
+                                    "Invalid string literal");
+            lexeme[i] = '\0';
+            type = STRING;
+            reader.GetNextCharacter();
+            break;
+         case READER<CALLBACKSUSED>::EOPC: 
+            {
+               static int count = 0;
+   
+               if ( ++count > (LOOKAHEAD+1) )
+                  ProcessCompilerError(sourceLineNumber,sourceLineIndex,
+                                       "Unexpected end-of-program");
+               else
+               {
+                  type = EOPTOKEN;
+                  reader.GetNextCharacter();
+                  lexeme[0] = '\0';
+               }
+            }
+            break;
+    /*     case '+':
+            type = PLUS;
+            lexeme[0] = nextCharacter; lexeme[1] = '\0';
+            reader.GetNextCharacter();
+            break;*/
+         case ':': 
+            type = COLON;
+            lexeme[0] = nextCharacter; lexeme[1] = '\0';
+            reader.GetNextCharacter();
+            break;
+//	<endl>
+         case '^': 
+         	//reader.GetLookAheadCharacter(1).character == 'n'
+         	//nextCharacter = reader.GetNextCharacter().character;
+         	if(reader.GetLookAheadCharacter(1).character == 'n')
+			{
+                //lexeme[i++] = nextCharacter;
+                //nextCharacter = reader.GetNextCharacter().character;				
+	            type = ENDL;
+	            lexeme[0] = nextCharacter; lexeme[1] = reader.GetLookAheadCharacter(1).character; lexeme[2] = '\0';
+	            reader.GetNextCharacter();
+	            reader.GetNextCharacter();
+	            break;
+            }
+// outshift
+         case '<': 
+         	//reader.GetLookAheadCharacter(1).character == 'n'
+         	//nextCharacter = reader.GetNextCharacter().character;
+         	if(reader.GetLookAheadCharacter(1).character == '<')
+			{
+                //lexeme[i++] = nextCharacter;
+                //nextCharacter = reader.GetNextCharacter().character;				
+	            type = OUTSHIFT;
+	            lexeme[0] = nextCharacter; lexeme[1] = reader.GetLookAheadCharacter(1).character; lexeme[2] = '\0';
+	            reader.GetNextCharacter();
+	            reader.GetNextCharacter();
+	            break;
+            }
+         default:  
+            type = UNKTOKEN;
+            lexeme[0] = nextCharacter; lexeme[1] = '\0';
+            reader.GetNextCharacter();
+            break;
+      }
+   }
+
+   tokens[LOOKAHEAD].type = type;
+   strcpy(tokens[LOOKAHEAD].lexeme,lexeme);
+   tokens[LOOKAHEAD].sourceLineNumber = sourceLineNumber;
+   tokens[LOOKAHEAD].sourceLineIndex = sourceLineIndex;
+
+#ifdef TRACESCANNER
+   sprintf(information,"At (%4d:%3d) token = %12s lexeme = |%s|",
+      tokens[LOOKAHEAD].sourceLineNumber,
+      tokens[LOOKAHEAD].sourceLineIndex,
+      TokenDescription(type),lexeme);
+   lister.ListInformationLine(information);
+#endif
+
+}
+
+//-----------------------------------------------------------
+const char *TokenDescription(TOKENTYPE type)
+//-----------------------------------------------------------
+{
+   int i;
+   bool isFound;
+   
+   isFound = false;
+   i = 0;
+   while ( !isFound && (i <= (sizeof(TOKENTABLE)/sizeof(TOKENTABLERECORD))-1) )
+   {
+      if ( TOKENTABLE[i].type == type )
+         isFound = true;
+      else
+         i++;
+   }
+   return ( isFound ? TOKENTABLE[i].description : "???????" );
 }
